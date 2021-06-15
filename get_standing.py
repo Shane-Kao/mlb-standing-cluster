0c2a7760fe772938cc9ca1956baac0bd632ce49b
shanekao <shanekao19870615@gmail.com>
2021-06-15 20:25:49 +0800

    init commit

---
+++ get_standing.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def get_standing():
+    r = requests.get(
+        url="https://statsapi.mlb.com/api/v1/standings",
+        params={
+            "leagueId": "103,104",
+            "season": 2021,
+            "standingsTypes": "regularSeason",
+        }
+    )
+    raw_data = r.json()
+    records = raw_data["records"]
+    team_records = sum([i["teamRecords"] for i in records], [])
+    standing = [{
+        "team_id": i["team"]["id"],
+        "team_name": i["team"]["name"],
+        "sport_games_back": i["sportGamesBack"],
+    } for i in team_records]
+    return standing
+
+
+if __name__ == "__main__":
+    data = get_standing()
+    for i in data:
+        print(i)
